130fc9afa420b89dc1e5170403dc3dc453ab5a8e
richardgrechko <162537734+richardgrechko@users.noreply.github.com>
2024-07-05 08:36:31 +0500

    Keep pitch

--- moviepy/video/fx/speedx.py
+++ moviepy/video/fx/speedx.py
@@ -1,21 +1,27 @@
 from moviepy.decorators import apply_to_mask,apply_to_audio
 
 
-def speedx(clip, factor = None, final_duration=None):
+def speedx(clip, factor=None, final_duration=None, keep_pitch=False):
     """
     Returns a clip playing the current clip but at a speed multiplied
-    by ``factor``. Instead of factor one can indicate the desired
+    by ``factor``. Instead of factor, one can indicate the desired
     ``final_duration`` of the clip, and the factor will be automatically
-    computed.
+    computed. If ``keep_pitch`` is True, the pitch will reset to 0.
     The same effect is applied to the clip's audio and mask if any.
     """
     
     if final_duration:
-        factor = 1.0* clip.duration / final_duration
+        factor = 1.0 * clip.duration / final_duration
         
-    newclip = clip.fl_time(lambda t: factor * t, apply_to=['mask', 'audio'])
+    newclip = clip.fl_time(lambda t: factor * t, apply_to=[apply_to_mask, apply_to_audio])
     
     if clip.duration is not None:
         newclip = newclip.set_duration(1.0 * clip.duration / factor)
+
+    if keep_pitch:
+        sr = clip.audio.fps
+        new_sr = newclip.audio.fps
+        pitch_change = int(np.round(12 * np.log2(new_sr / sr)))
+        newclip.audio = newclip.audio.fx(pitch, pitch_change)
     
     return newclip
